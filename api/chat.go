504118f0e169d409423258abaf30f3f1a1a85fbd
EdisonP <133362311+edions@users.noreply.github.com>
2025-05-24 18:19:23 +0800

    refactor: improve chat

--- api/chat.go
+++ api/chat.go
@@ -3,6 +3,8 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 	"waterfalls/dto"
 
@@ -11,7 +13,13 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-var Clients = make(map[*websocket.Conn]bool)
+type Client struct {
+	Conn           *websocket.Conn
+	ConversationID string
+}
+
+var Clients = make(map[*websocket.Conn]*Client)
+var ClientsMutex sync.RWMutex
 var Broadcast = make(chan dto.Message)
 
 var upgrader = websocket.Upgrader{
@@ -23,41 +31,247 @@ var upgrader = websocket.Upgrader{
 func HandleMessages(db *sqlx.DB) {
 	for {
 		msg := <-Broadcast
-		msg.Timestamp = time.Now()
 
-		for client := range Clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				fmt.Println("Error writing message to client:", err)
-				client.Close()
-				delete(Clients, client)
+		ClientsMutex.RLock()
+		for conn, client := range Clients {
+			if client.ConversationID == msg.ConversationId {
+				err := conn.WriteJSON(msg)
+				if err != nil {
+					fmt.Println("Error writing message to client:", err)
+					conn.Close()
+					delete(Clients, conn)
+				}
 			}
 		}
+		ClientsMutex.RUnlock()
 	}
 }
 
 func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
+	r.GET("/chat/:convo_id/messages", func(c *gin.Context) {
+		convoId := c.Param("convo_id")
+
+		query := `
+			SELECT
+				message_id,
+				sender_id,
+				conversation_id,
+				sender_name,
+				role,
+				content,
+				timestamp 
+			FROM messages
+			WHERE conversation_id = ?
+			ORDER BY timestamp ASC`
+
+		var messages []dto.MessageEntity
+		err := db.Select(&messages, query, convoId)
+		if err != nil {
+			fmt.Println("WebSocket read error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve messages"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"messages": messages})
+	})
+
+	r.GET("/chat/client/:id/messages", func(c *gin.Context) {
+		clientId := c.Param("id")
+
+		var convoId string
+		err := db.Get(&convoId, "SELECT conversation_id FROM conversations WHERE user_id = ? AND staff_only = false", clientId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get conversation ID: " + err.Error()})
+			return
+		}
+
+		query := `
+			SELECT
+				message_id,
+				sender_id,
+				conversation_id,
+				sender_name,
+				role,
+				content,
+				timestamp 
+			FROM messages
+			WHERE conversation_id = ?
+			ORDER BY timestamp ASC`
+
+		var messages []dto.MessageEntity
+		err = db.Select(&messages, query, convoId)
+		if err != nil {
+			fmt.Println("DB select error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve messages"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"messages": messages, "convo_id": convoId})
+	})
+
+	r.GET("/chat/conversation/:area_id", func(c *gin.Context) {
+		areaIdStr := c.Param("area_id")
+		areaId, err := strconv.Atoi(areaIdStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid area_id"})
+			return
+		}
+
+		agentIdStr := c.Query("uid")
+		agentId, err := strconv.Atoi(agentIdStr)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid uid"})
+			return
+		}
+
+		query := `
+			WITH admin_info AS (
+				SELECT CONCAT(firstname, ' ', lastname) AS fullname, role
+				FROM account_staffs
+				WHERE role = 'Admin'
+				LIMIT 1
+			),
+
+			latest_messages AS (
+				SELECT m1.*
+				FROM messages m1
+				JOIN (
+					SELECT conversation_id, MAX(timestamp) AS latest_timestamp
+					FROM messages
+					GROUP BY conversation_id
+				) latest
+				ON m1.conversation_id = latest.conversation_id
+				AND m1.timestamp = latest.latest_timestamp
+			)
+
+			SELECT
+				m.message_id,
+				m.sender_id,
+				c.conversation_id,
+				m.sender_name AS last_sender,
+				m.content,
+				m.timestamp,
+				ar.area AS area_name,
+				CONCAT(a.firstname, ' ', a.lastname) AS fullname,
+				a.role,
+				a.type AS customer_type
+			FROM conversations c
+			LEFT JOIN account_clients a ON c.user_id = a.client_id
+			LEFT JOIN areas ar ON a.area_id = ar.id
+			LEFT JOIN latest_messages m ON c.conversation_id = m.conversation_id
+			WHERE c.staff_only = false AND c.area_id = ?
+
+			UNION ALL
+
+			SELECT
+				m.message_id,
+				m.sender_id,
+				c.conversation_id,
+				m.sender_name AS last_sender,
+				m.content,
+				m.timestamp,
+				NULL AS area_name,
+				ai.fullname,
+				ai.role,
+				NULL AS customer_type
+			FROM conversations c
+			JOIN admin_info ai
+			LEFT JOIN latest_messages m ON c.conversation_id = m.conversation_id
+			WHERE c.staff_only = true AND c.user_id = ?
+
+			ORDER BY timestamp DESC`
+
+		var conversations []dto.ConversationEntity
+		err = db.Select(&conversations, query, areaId, agentId)
+		if err != nil {
+			fmt.Println("DB query error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve messages"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"conversations": conversations})
+	})
+
+	r.GET("/chat/conversation", func(c *gin.Context) {
+		query := `
+			WITH latest_messages AS (
+				SELECT m1.*
+				FROM messages m1
+				JOIN (
+					SELECT conversation_id, MAX(timestamp) AS latest_timestamp
+					FROM messages
+					GROUP BY conversation_id
+				) latest ON m1.conversation_id = latest.conversation_id
+						AND m1.timestamp = latest.latest_timestamp
+			),
+			users AS (
+				SELECT
+					ac.client_id AS user_id,
+					CONCAT(ac.firstname, ' ', ac.lastname) AS fullname,
+					ac.role,
+					ac.type AS customer_type,
+					ar.area AS area_name,
+					'client' AS user_type
+				FROM account_clients ac
+				LEFT JOIN areas ar ON ac.area_id = ar.id
+
+				UNION ALL
+
+				SELECT
+					ast.staff_id AS user_id,
+					CONCAT(ast.firstname, ' ', ast.lastname) AS fullname,
+					ast.role,
+					NULL AS customer_type,
+					ar.area AS area_name,
+					'staff' AS user_type
+				FROM account_staffs ast
+				LEFT JOIN areas ar ON ast.area_id = ar.id
+			)
+
+			SELECT
+				m.message_id,
+				m.sender_id,
+				c.conversation_id,
+				m.sender_name AS last_sender,
+				m.content,
+				m.timestamp,
+				u.area_name,
+				u.fullname,
+				u.role,
+				u.customer_type
+			FROM conversations c
+			JOIN users u ON c.user_id = u.user_id AND (
+				(u.user_type = 'client' AND c.staff_only = FALSE) OR
+				(u.user_type = 'staff' AND c.staff_only = TRUE)
+			)
+			LEFT JOIN latest_messages m ON c.conversation_id = m.conversation_id
+			ORDER BY m.timestamp DESC`
+
+		var conversations []dto.ConversationEntity
+		err := db.Select(&conversations, query)
+		if err != nil {
+			fmt.Println("DB query error:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve messages"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"conversations": conversations})
+	})
+
+	// OLD API
+
 	r.POST("/send_message", func(c *gin.Context) {
 		var msg dto.Message
 		if err := c.ShouldBindJSON(&msg); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid message format"})
 			return
 		}
-
-		// Insert message into the database using sqlx's NamedExec
-		query := "INSERT INTO messages (user_id, area_id, content, timestamp) VALUES (:user_id, :area_id, :content, :timestamp)"
-		_, err := db.NamedExec(query, map[string]interface{}{
-			"user_id":    msg.SenderId,
-			"area_id": msg.AreaId,
-			"content":   msg.Content,
-			"timestamp": time.Now(),
-		})
+		query := "INSERT INTO messages (user_id, conversation_id, content) VALUES (:user_id, :conversation_id, :content)"
+		_, err := db.NamedExec(query, msg)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to store message"})
 			return
 		}
-
-		// Broadcast the message to all connected clients
 		Broadcast <- msg
 
 		c.JSON(http.StatusOK, gin.H{"status": "Message sent"})
@@ -79,7 +293,7 @@ func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
 			LEFT JOIN account_clients c ON m.sender_id = c.client_id
 			WHERE m.customer = ?
 			ORDER BY timestamp ASC`
-		
+
 		var messages []dto.MessageEntity
 		err := db.Select(&messages, query, username)
 		if err != nil {
@@ -97,18 +311,15 @@ func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
 		query := `
 			SELECT
 				message_id,
-				m.customer,
-				m.sender_id,
-				m.area_id,
-				CONCAT(c.firstname, ' ', c.lastname) AS fullname,
+				sender_id,
+				conversation_id,
+				sender_name,
 				content,
 				timestamp 
-			FROM messages m
-			LEFT JOIN account_clients c ON m.sender_id = c.client_id
-			WHERE m.area_id = ?
-			GROUP BY m.customer
+			FROM messages
+			WHERE conversation_id = ?
 			ORDER BY timestamp DESC`
-		
+
 		var messages []dto.MessageEntity
 		err := db.Select(&messages, query, areaId)
 		if err != nil {
@@ -141,7 +352,7 @@ func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
 				ON m.customer = latest_messages.customer
 				AND m.timestamp = latest_messages.latest_timestamp
 			ORDER BY m.timestamp ASC`
-		
+
 		var messages []dto.MessageEntity
 		err := db.Select(&messages, query)
 		if err != nil {
@@ -154,15 +365,31 @@ func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
 	})
 
 	r.GET("/chat", func(c *gin.Context) {
+		ConversationID := c.Query("convo_id")
+		if ConversationID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Conversation not Found"})
+			return
+		}
+
 		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
 			fmt.Println("WebSocket upgrade error:", err)
 			return
 		}
 
-		Clients[conn] = true
+		client := &Client{
+			Conn:           conn,
+			ConversationID: ConversationID,
+		}
+
+		ClientsMutex.Lock()
+		Clients[conn] = client
+		ClientsMutex.Unlock()
+
 		defer func() {
+			ClientsMutex.Lock()
 			delete(Clients, conn)
+			ClientsMutex.Unlock()
 			conn.Close()
 		}()
 
@@ -178,22 +405,22 @@ func ChatRoutes(r *gin.Engine, db *sqlx.DB) {
 			}
 
 			query := `
-				INSERT INTO messages (sender_id, area_id, customer, content, timestamp)
-				VALUES (:sender_id, :area_id, :customer, :content, :timestamp)`
+            INSERT INTO messages (sender_id, sender_name, role, content, conversation_id, timestamp)
+            VALUES (:sender_id, :sender_name, :role, :content, :conversation_id, :timestamp)`
 
-			_, err = db.NamedExec(query, map[string]interface{}{
-				"sender_id":    msg.SenderId,
-				"area_id": msg.AreaId,
-				"customer": msg.Customer,
-				"content":   msg.Content,
-				"timestamp": time.Now(),
+			_, err = db.NamedExec(query, map[string]any{
+				"sender_id":       msg.SenderId,
+				"sender_name":     msg.SenderName,
+				"role":            msg.Role,
+				"content":         msg.Content,
+				"conversation_id": ConversationID,
+				"timestamp":       time.Now(),
 			})
 			if err != nil {
 				conn.WriteJSON(gin.H{"error": "Failed to store message"})
 				continue
 			}
-
-			// Broadcast the message
+			msg.ConversationId = ConversationID
 			Broadcast <- msg
 		}
 	})
