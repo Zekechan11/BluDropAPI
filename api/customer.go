0e14ad9b215a93664ab9a2dfd619a107ee4feefc
EdisonP <133362311+edions@users.noreply.github.com>
2025-05-26 05:43:41 +0800

    fix: total loan ammount not accurate

--- api/customer.go
+++ api/customer.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	"waterfalls/dto"
 
@@ -11,41 +12,57 @@ import (
 func CustomerRoutes(r *gin.Engine, db *sqlx.DB) {
 
 	r.GET("/v2/api/dashboard/:client_id", func(ctx *gin.Context) {
-		client_id := ctx.Param("client_id")
-	
+		clientID := ctx.Param("client_id")
+
 		queryContainers := `
 			SELECT total_containers_on_loan 
 			FROM containers_on_loan
 			WHERE customer_id = ?
 		`
 		var col int
-		err := db.Get(&col, queryContainers, client_id)
+		err := db.Get(&col, queryContainers, clientID)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+			if err == sql.ErrNoRows {
+				col = 0
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
 		}
-	
+
 		queryLoan := `
-			SELECT SUM(total_price - payment) 
+			WITH overpayments AS (
+			SELECT 
+				customer_id,
+				SUM(payment - total_price) AS overpayment
+			FROM customer_order
+			WHERE customer_id = ?
+			GROUP BY customer_id
+			)
+			SELECT
+			SUM(total_price - payment) - COALESCE((SELECT overpayment FROM overpayments WHERE customer_id = ?), 0) AS total_unpaid
 			FROM customer_order
 			WHERE customer_id = ?
 		`
-	
 		var totalLoan float64
-		err = db.Get(&totalLoan, queryLoan, client_id)
+		err = db.Get(&totalLoan, queryLoan, clientID, clientID, clientID)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+			if err == sql.ErrNoRows {
+				totalLoan = 0
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
 		}
-	
+
 		ctx.JSON(http.StatusOK, gin.H{
-			"col": col,
+			"col":  col,
 			"loan": totalLoan,
 		})
 	})
 
-	r.GET("/v2/api/orders/:client_id", func (ctx *gin.Context){
-	client_id := ctx.Param("client_id")
+	r.GET("/v2/api/orders/:client_id", func(ctx *gin.Context) {
+		client_id := ctx.Param("client_id")
 		query := `
 		SELECT
 			id,
@@ -56,13 +73,13 @@ func CustomerRoutes(r *gin.Engine, db *sqlx.DB) {
 		FROM customer_order
 		WHERE customer_id = ?
 	`
-	var agents []dto.CustomerTransaction
-	err := db.Select(&agents, query, client_id)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+		var agents []dto.CustomerTransaction
+		err := db.Select(&agents, query, client_id)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-	ctx.JSON(http.StatusOK, agents)
+		ctx.JSON(http.StatusOK, agents)
 	})
 }
